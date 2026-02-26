334492c42be190aa5809ba28ac64199c6cbd4a84
Stefan Strand <stefanboytchev@hotmail.com>
2026-02-26 14:59:31 +0100

    connected frontend to backedn

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/health", (req, res) => {
     res.json("Hello World!");
+});
+
+app.post("/ehco", (req, res) => {
+    res.json({ received: req.body });
 });
 
 app.listen(port, () => {
