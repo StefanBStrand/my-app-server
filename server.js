b2243851740976fa4a73c1d43e27bde53b6799b3
Stefan Strand <stefanboytchev@hotmail.com>
2026-04-21 13:53:22 +0200

    altered port to take .env from REnder

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require("./app"); // importing the Express app configured in app.js. THis gives fully prepared app (routes, middleware.etc)
 // app = the Express application!
 
-const port = 4000; // defining which port the server should run/listen on.
+const port = process.env.PORT || 4000; // defining which port the server should run/listen on.
+// PORT from Render hosting service or local 4000...
 
 // starting the server and listening for incoming HTTP requests. app.listen tells node.js "start listeing for incoming requests"
 app.listen(port, () => {
